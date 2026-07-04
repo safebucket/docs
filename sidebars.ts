fe70826648bf001278c89a6e0be44d1b08b8771c
Renizmy <remi.beltramini@gmail.com>
2026-07-04 19:57:25 +0200

    fix

--- sidebars.ts
+++ sidebars.ts
@@ -1,4 +1,5 @@
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+import apiSidebar from './docs/api/sidebar';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -102,6 +103,7 @@ const sidebars: SidebarsConfig = {
       collapsed: false,
     },
   ],
+  apiSidebar,
 };
 
 export default sidebars;
